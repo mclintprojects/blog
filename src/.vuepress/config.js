2f54ec4564d72e93e5c376d2a41098e289f69656
Mbah Clinton <clintonmbah44@gmail.com>
2019-05-01 09:20:21 +0000

    added vuepress config

---
+++ src/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	title: 'Mbah Clinton',
+	description:
+		'Full-stack software engineer specializing in web and mobile applications. Kotlin/Android, JavaScript/Vue/Node, and Ruby/Rails 5.'
+};
